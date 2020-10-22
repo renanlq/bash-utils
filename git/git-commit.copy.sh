b6cd212c052a191db65ff07a50ab617c495ff1d6
Renan Queiroz <renanlq@gmail.com>
2020-10-22 15:47:43 -0300

    Create git-commit.copy.sh

---
+++ git/git-commit.copy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Diretório de destino
+PATH=$3
+
+echo "Copiando pastas e arquivos de: $PATH"
+
+for i in $(git diff --name-only $1 $2)
+    do
+        # Criando diretório de destino
+        mkdir -p "$PATH/$(dirname $i)"
+         # Copiando arquivos
+        cp "$i" "$PATH/$i"
+    done
+
+echo "Arquivos copiados para o diretório";
